b1d1ccda2ef5c004137b755c85435dcf1b6cdda6
Jon Hinks <projects.ws@jondh.me.uk>
2017-04-21 09:50:17 +0100

    Get skeleton integration test working

--- test/integration/tests/CrawlTest.php
+++ test/integration/tests/CrawlTest.php
@@ -4,17 +4,21 @@
  * Checks the crawler, on a real site, without using a proxy
  */
 
-namespace Proximate\Tests\Integration;
+namespace Proximate\Test\Integration;
 
 use halfer\SpiderlingUtils\NamespacedTestCase;
 
 class CrawlTest extends NamespacedTestCase
 {
+    const URL_PREFIX = 'http://localhost:10000';
+
     /**
      * @driver simple
      */
     public function testSomething()
     {
-        $this->markTestIncomplete();
+        // @todo Swap this for a run of the crawler
+        $text = $this->visit(self::URL_PREFIX . '/site1/page1.html')->text();
+        $this->assertContains('Linky', $text);
     }
 }
