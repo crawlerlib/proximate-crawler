4f63e26dbe21f967c16411c819edc8dad3b3d589
Jon Hinks <projects.ws@jondh.me.uk>
2017-04-21 01:00:06 +0100

    Add skeleton PHPUnit integration test

---
+++ test/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Sets up autoloading before starting tests
+ */
+
+$projectRoot = realpath(__DIR__ . '/..');
+require_once $projectRoot . '/vendor/autoload.php';
---
+++ test/integration/tests/CrawlTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Checks the crawler, on a real site, without using a proxy
+ */
+
+namespace Proximate\Tests\Integration;
+
+use halfer\SpiderlingUtils\NamespacedTestCase;
+
+class CrawlTest extends NamespacedTestCase
+{
+    /**
+     * @driver simple
+     */
+    public function testSomething()
+    {
+        $this->markTestIncomplete();
+    }
+}
